812dd8e46c0f67fdd51579f20372fc71eb1e5a5f
LOG2144062 <LOG2144062@maricopa.edu>
2024-04-30 19:41:52 -0700

    Master branch

---
+++ The Dominion of Kings.cs
@@ -0,0 +1,68 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/* 
+Date: 04/18/2024
+Page #: 57
+Challenge: The Dominion of Kings
+EXP: 100 XP
+
+ Objectives:
+• Create a program that allows users to enter how many provinces, duchies, and estates they have.
+• Add up the user's total score, giving 1 point per estate, 3 per duchy, and 6 per province.
+• Display the point total to the user.
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+using System;
+
+string King1 = "Melik";
+string King2 = "Casik";
+string King3 = "Balik";
+
+int King1_Points, King2_Points, King3_Points;
+string King1_estates, King1_duchies, King1_provinces, King2_estates, King2_duchies, King2_provinces, King3_estates, King3_duchies, King3_provinces;
+int King1_estates_int, King1_duchies_int, King1_provinces_int, King2_estates_int, King2_duchies_int, King2_provinces_int, King3_estates_int, King3_duchies_int, King3_provinces_int;
+
+int _estateValue = 1;
+int _duchyValue = 3;
+int _provinceValue = 6;
+
+Console.WriteLine(King1 + ", please enter how many estates you have: ");
+King1_estates = Console.ReadLine();
+King1_estates_int = Convert.ToInt32(King1_estates);
+Console.WriteLine(King1 + ", please enter how many duchies you have: ");
+King1_duchies = Console.ReadLine();
+King1_duchies_int = Convert.ToInt32(King1_duchies);
+Console.WriteLine(King1 + ", please enter how many provinces you have: ");
+King1_provinces = Console.ReadLine();
+King1_provinces_int = Convert.ToInt32(King1_provinces);
+
+Console.WriteLine(King2 + ", please enter how many estates you have: ");
+King2_estates = Console.ReadLine();
+King2_estates_int = Convert.ToInt32(King2_estates);
+Console.WriteLine(King2 + ", please enter how many duchies you have: ");
+King2_duchies = Console.ReadLine();
+King2_duchies_int = Convert.ToInt32(King2_duchies);
+Console.WriteLine(King2 + ", please enter how many provinces you have: ");
+King2_provinces = Console.ReadLine();
+King2_provinces_int = Convert.ToInt32(King2_provinces);
+
+Console.WriteLine(King3 + ", please enter how many estates you have: ");
+King3_estates = Console.ReadLine();
+King3_estates_int = Convert.ToInt32(King3_estates);
+Console.WriteLine(King3 + ", please enter how many duchies you have: ");
+King3_duchies = Console.ReadLine();
+King3_duchies_int = Convert.ToInt32(King3_duchies);
+Console.WriteLine(King3 + ", please enter how many provinces you have: ");
+King3_provinces = Console.ReadLine();
+King3_provinces_int = Convert.ToInt32(King3_provinces);
+
+King1_Points = (King1_estates_int * _estateValue) + (King1_duchies_int * _duchyValue) + (King1_provinces_int * _provinceValue);
+King2_Points = (King2_estates_int * _estateValue) + (King2_duchies_int * _duchyValue) + (King2_provinces_int * _provinceValue);
+King3_Points = (King3_estates_int * _estateValue) + (King3_duchies_int * _duchyValue) + (King3_provinces_int * _provinceValue);
+
+Console.WriteLine("King " + King1 + " has " + King1_Points + " points.");
+Console.WriteLine("King " + King2 + " has " + King2_Points + " points.");
+Console.WriteLine("King " + King3 + " has " + King3_Points + " points.");
+
+
+
---
+++ The Variable Shop.cs
@@ -0,0 +1,61 @@
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
+Date: 04/14/2024
+Page #: 45
+Challenge: The Variable Shop
+EXP: 100 XP
+
+ Objectives:
+• Build a program with a variable of all fourteen types described in this level.
+    [✓] int
+    [✓] short
+    [✓] long
+    [✓] byte
+    [✓] sbyte
+    [✓] uint
+    [✓] ushort
+    [✓] ulong
+    [✓] Char
+    [✓] String   
+    [✓] Float
+    [✓] Double
+    [✓] Decimal
+    [✓] Bool
+• Assign each of them a value using a literal of the correct type.
+• Use Console.WriteLine to display the contents of each variable.
+ */
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+ int age = 28;
+short januaryDays = 31;
+long earthCircumference = 24_901;
+byte binary = 2;
+sbyte temperature = 14;
+uint numberOfStates = 50;
+ushort imagePixels = 1250;
+ulong totalAtoms = 1_000_000;
+char Confirm = 'y';
+string firstName = "Logan";
+float gramsToOunces = 28.3495f;
+double valueOfPi = 3.141579;
+decimal gasPricePerGallon = 4.54m;
+bool legalAge = true;
+
+Console.WriteLine(
+    "Values are as follows:" +
+    "\n intAge = " + age + 
+    "\n Days in January = " + januaryDays +
+    "\n Earth's circumference = " + earthCircumference + 
+    "\n Binary value = " + binary +
+    "\n Temperature = " + temperature +
+    "\n  = " + numberOfStates +
+    "\n # of Pixels = " + imagePixels +
+    "\n Atoms in a hair follice = " + totalAtoms +
+    "\n Character for Yes = " + Confirm +
+    "\n My own first name = " + firstName +
+    "\n Grams to ounces = " + gramsToOunces +
+    "\n The value of pi = " + valueOfPi +
+    "\n The price of gas per gallon = " + gasPricePerGallon +
+    "\n Am I of legal age? = " + legalAge +
+    "\n " );
