dfe852d10e0d76967fe2600e91af7927d80c8a22
LOG2144062 <LOG2144062@maricopa.edu>
2024-06-02 12:53:03 -0700

    Finished the Manticore challenge.

--- Main.cs
+++ Main.cs
@@ -1,49 +1,128 @@
 ﻿///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
-Date: 05/26/2024
-Page #: 106
-Challenge: Taking a Number
-EXP: 100XP
+Date: 06/02/2024
+Page #: 101248
+Challenge: Hunting the Manticore
+EXP: 250XP
 
  Objectives:
-• Make a method with the signautre int askForNumber(string text). Display the text paramter in the console window, get a response from the user, 
-  convert it to an int, and return it. This might look like this: int result = AskForNumber("What is the airspeed velocity of an unladen swallow?");
-• Make a method with the signature int AskforNumberInRange(string text, int min, int max). Only return if the entered number is between the min and max values.
-• Place these methods in at least one of your previous programs to improve it.
+• Ask the player to choose the Manticore's distance.
+• Display round #, City HP, and Manticore HP.
+• Have the player attempt to find the distance through cannon shots.
+• Calculate the damage done by the cannon through the modulus operator.
+• The city should take 1 damage for every overshot or undershot.
+• Terminate when either party reaches 0 health points.
  */
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 using System;
-////////////////////////////////////////////////////////////////////////////
-class TakingANumber
-{
-    static void AskForNumber()
-    {
-        bool isValid = false;
-        int result2 = 0;
-        while (!isValid)
-        {
-            Console.WriteLine("Please insert a number:");
-            string result = Console.ReadLine();
-            result2 = Convert.ToInt32(result);
 
-            if (result2 >= 10 || result2 <= 0)
-            {
-                isValid = false;
-                Console.WriteLine("Invalid entry;");
-                Console.WriteLine("Entry must be between 0 and 10.");
-            }
-            else if (result2 >= 0 && result2 <= 10)
-            {
-                isValid = true;
-                Console.WriteLine("The number entered was: " + result2);
-                return;
-            }
+class HuntingManticore
+{
+    // Declare variables at the class level!
+    int ManticoreHealth = 10;
+    int CityHealth = 15;
+    int Round = 0;
+    int CannonDamage = 0;
+    int TilesInt = 0;
+    int CannonShot = 0;
+
+    public static void Main(string[] args)
+    {
+        HuntingManticore game = new HuntingManticore();
+        game.StartGame();
+    }
+
+    void StartGame()
+    {
+        // Declare distance of ship and clear screen.
+        Console.WriteLine("How many tiles away is the Manticore? (0-100)");
+        string TilesString = Console.ReadLine();
+        TilesInt = Convert.ToInt32(TilesString);
+        Console.Clear();
+
+        // "Only stop when someone dies."
+        while (ManticoreHealth > 0 && CityHealth > 0)
+        {
+            RoundIncrease();
+            CaculateCannonDamage();
+            Console.WriteLine("STATUS: Round " + Round + "| City HP: " + CityHealth + "| Manticore HP: " + ManticoreHealth);
+            Console.WriteLine("The cannon is expected to deal " + CannonDamage + " damage this round.");
+            Console.WriteLine("Enter desired cannon range: ");
+            string ShotString = Console.ReadLine();
+            CannonShot = Convert.ToInt32(ShotString);
+            DetermineCannotShot();
+
+        }
+
+    }
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    void RoundIncrease()
+    {
+        Round++;
+    }
+    void CaculateCannonDamage()
+    {
+        if (Round % 3 == 0 && Round % 5 == 0) // If round # is a multiple of 3 AND 5, fire electric damage of 10 is done.
+
+        {
+            CannonDamage = 10;
+            return;
+        }
+        else if (Round % 3 == 0 || Round % 5 == 0) // If round # is a multiple of 3 OR 5, fire or electric damage of 3 is done.
+        {
+            CannonDamage = 3;
+            return;
+        }
+        else
+        {
+            CannonDamage = 1;
+            return;
         }
     }
-    ////////////////////////////////////////////////////////////////////////////
-    static void Main(string[] args)
+    void DetermineCannotShot()
     {
-        AskForNumber();
+        if (CannonShot > TilesInt) // 
+        {
+            CityHealth = CityHealth - 1;
+            if (CityHealth <= 0)
+            {
+                Console.WriteLine("Oh no! The city has fallen! Retreat!");
+            }
+            else
+            {
+                Console.WriteLine("That round OVERSHOT the target!");
+            }
+            return;
+        }
+        else if (CannonShot < TilesInt) // 
+        {
+            CityHealth = CityHealth - 1;
+            if (CityHealth <= 0)
+            {
+                Console.WriteLine("Oh no! The city has fallen! Retreat!");
+            }
+            else
+            {
+                Console.WriteLine("That round UNDERSHOT the target!");
+            }
+            return;
+
+        }
+        else
+        {
+            ManticoreHealth = ManticoreHealth - CannonDamage;
+            if (ManticoreHealth <= 0)
+            {
+                Console.WriteLine("Hooray! The Manticore has fallen!");
+            }
+            else
+            {
+                Console.WriteLine("That round was a DIRECT HIT for " + CannonDamage + " damage!");
+            }
+
+            return;
+        }
     }
+
 }
