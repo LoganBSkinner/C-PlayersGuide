c4d89f2a24a4e3a8d8268f6d83782410fcbe84bd
LOG2144062 <LOG2144062@maricopa.edu>
2024-09-29 13:36:55 -0700

    Using ChatGPT as my analyst to create a Vending machine class.

--- Main.cs
+++ Main.cs
@@ -5,9 +5,6 @@ Requirements met:
 
  [x] Allow the vending machine to display a list of available products.
  [x] Allow the user to input products into the vending machine.
-
-Requirements not met:
-
  [ ] Associate each product with a price.
  [ ] Track the quantity of each product (stocking).
  [ ] Implement a way for users to buy products from the vending machine.
@@ -18,19 +15,40 @@ Requirements not met:
  [ ] Simulate returning change after a purchase.
  [ ] Provide a menu for user interaction (view menu, insert money, buy product, exit).
  [ ] Implement loops to keep the program running until the user exits.
+
+Feedback from Ben:
+
+ [x] Change file names to something that more closely follows best practice.
+ [x] Update C# comments to use triple slashs "///".
+ [x] Make fixes to case of fields and properties to more closely follow best practice.
+ [x] Rename the Menu array to something more specific (i.e avoid code smell).
+ [ ] Resolve debugging scenario that Ben proposed (i.e You have an array with an allocated space of 5. The 0th item is null and the 1st item is NOT null. What would happen in this case?
+ [ ] Remove artifacts that Ben called out.
+ [ ] Approach the array setup for my vending machine a little differently using a list or dictionary.
+
 */
 
+using System.Linq.Expressions;
 using VendingMachineManager;
 public class Program
 {
     public static void Main(string[] args)
     {
         VendingMachine vendingMachine = new VendingMachine();
-        vendingMachine.UpdateMenu();
-        Console.WriteLine("Vending machine inventory:");
-        vendingMachine.ReadMenu();
+        Console.WriteLine("--------------------\nVending Machine Menu\nPlease input your user type:\n(1) Operator\n--------------------");
+        string Response = Console.ReadLine();
+        int userType = Convert.ToInt32(Response);
+        switch (userType)
+        {
+            case 1:
+                //(1) Operator                
+                vendingMachine.ReadOperatorMenu();// call method to read the operator's menu.
+                break;
+            default:
+                Console.WriteLine("No valid option selected.\nClosing...");
+                break;
+        }
     }
 
 }
-
 
---
+++ MyFiles/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_PlayersGuide
+{
+    static class Configuration
+    {
+        public static int VendingMachineCapacity { get; } = 20;
+    }
+}
